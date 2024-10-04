93019837a64d21dcec9a41565e61c4b49a1ecf72
kbrum <kaykyazevedo97@gmail.com>
2024-10-04 18:55:25 -0300

    Primeiras funcionabilidades

--- main.py
+++ main.py
@@ -24,32 +24,34 @@ def exibir_opcoes():
     
 
 def Conferir_alternativas():
-    while True:
-        escolher_opcoes = int(input('Escolha uma opção: '))
-        
-        if escolher_opcoes == 1:
-            os.system('cls')
-            main_produtos()
+    
+    escolher_opcoes = int(input('Escolha uma opção: '))
             
-        elif escolher_opcoes == 2:
-            os.system('cls')
-            main_estoque()
+    if escolher_opcoes == 1:
+        os.system('cls')
+        main_produtos()
+                
+    elif escolher_opcoes == 2:
+        os.system('cls')
+        main_estoque()
+                
+    elif escolher_opcoes == 3:
+        os.system('cls')
+        main_financeiro()
+                
+    elif escolher_opcoes == 4:
+        os.system('cls')
+        main_funcionarios()
+                
+    elif escolher_opcoes == 5:
+        os.system('cls')
+        print('Finalizado')
             
-        elif escolher_opcoes == 3:
-            os.system('cls')
-            main_financeiro()
+    elif escolher_opcoes != int:
+        print('Digite apenas numeros')
             
-        elif escolher_opcoes == 4:
-            os.system('cls')
-            main_funcionarios()
-            
-        elif escolher_opcoes == 5:
-            os.system('cls')
-            print('Finalizado')
-            break
-            
-        else:
-            print("Por favor, selecione uma opção valida")
+    else:
+        print("Por favor, selecione uma opção valida")
         
         
 def main():
